761b47edf1b6b8d62a336a7274701945b79149f4
nen.phan <thanhnen1360@gmail.com>
2025-05-17 21:26:02 +0700

    update home slide

--- lib/widgets/home_banner_widget.dart
+++ lib/widgets/home_banner_widget.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/route_manager.dart';
+import 'package:hkcoin/data.models/product.dart';
 import 'package:hkcoin/data.models/slide.dart';
+import 'package:hkcoin/presentation.controllers/home_body_controller.dart';
+import 'package:hkcoin/presentation.pages/product_detail_page.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class HomeSlideWidget extends StatefulWidget {
@@ -39,49 +43,64 @@ class _HomeSlideWidgetState extends State<HomeSlideWidget> {
         children: List.generate(widget.slides.length, (index) {
           String url = widget.slides[index].image?.thumbUrl ?? "";
           return GestureDetector(
-            onTap: () => {
-                if(widget.slides[index].route !=null){
+            onTap: () {
+              if (widget.slides[index].route != null) {
+                if (widget.slides[index].route!.routeName == "/product") {
+                  var homeBodyController = Get.find<HomeBodyController>();
+                  Product? product = homeBodyController.products
+                      .firstWhereOrNull(
+                        (e) => e.id == widget.slides[index].route!.routeId,
+                      );
+                  if (product != null) {
+                    Get.toNamed(
+                      ProductDetailPage.route,
+                      arguments: ProductDetailPageParam(product: product),
+                    );
+                  }
+                } else {
                   Get.toNamed(
-                    widget.slides[index].route!.routeName??"",
+                    widget.slides[index].route!.routeName ?? "",
                     arguments: widget.slides[index].route!.routeId,
-                  )                 
+                  );
                 }
-                else{
-                  _launchUrl(widget.slides[index].slideUrl)
-                }
+              } else {
+                _launchUrl(widget.slides[index].slideUrl);
+              }
             },
             child: Container(
               margin: const EdgeInsets.symmetric(horizontal: 5),
               child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: SizedBox(
-                // width: 150,
-                // height: 150,
-                child: Image.network(
-                  url.contains("http") ? url : "https:$url",
-                  fit: BoxFit.cover,                 
+                borderRadius: BorderRadius.circular(10),
+                child: SizedBox(
+                  // width: 150,
+                  // height: 150,
+                  child: Image.network(
+                    url.contains("http") ? url : "https:$url",
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             ),
-            ),
-          );          
+          );
         }),
       ),
     );
   }
+
   Future<void> _launchUrl(String url) async {
-    try {      
-      if (await canLaunch(url)) {        
+    try {
+      if (await canLaunch(url)) {
         await launch(url);
       } else {
         // Xử lý khi không thể mở URL
-      _showSnackBar('Could not launch $url', Colors.red);         
+        _showSnackBar('Could not launch $url', Colors.red);
       }
-    } catch (e) {      
-      _showSnackBar('Error: ${e.toString()}', Colors.red);   
+    } catch (e) {
+      _showSnackBar('Error: ${e.toString()}', Colors.red);
     }
   }
-   void _showSnackBar(String message, Color color) {
+
+  void _showSnackBar(String message, Color color) {
     if (!mounted) return;
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
